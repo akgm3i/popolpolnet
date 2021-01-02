e75bccb9dbd321e83f017cf5443d189198876354
akgm3i <akgm3i@users.noreply.github.com>
2021-01-02 10:41:25 +0000

    deploy: 6a2e4ed76966456f04cc1c044d84717d23cce735

--- _next/static/5t3Xu0CYBEYS5eBxnzrYC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc39b4881d3c06665c5d4.css","static\u002Fchunks\u002Fpages\u002Findex-3bc2f55064843377e366.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85785763783c1e33a9f3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
